d4a65c1d1e41b0394b1fe003315f70a0fc134eee
CiscoVaz <saberassasin21@gmail.com>
2025-07-18 23:53:28 +0000

    Further with main

--- Main.java
+++ Main.java
@@ -27,7 +27,44 @@ public class Main {
             String status = sc.next();
             System.out.println("Category (0=Work,1=School,2=Personal,3=Errand,4=Other): ");
             int catIdx = Utility.getInt(sc, "");
-
+            if (catIdx < 0 || catIdx >= tm.categories.length) catIdx = 4;
+            System.out.print("Recurring? (y/n): ");
+            boolean recur = sc.next().equalsIgnoreCase("y");
+            int recurCount = 0;
+            if (recur) recurCount = Utility.getInt(sc, "How many times? ");
+            Task t = new Task(title, due, priority, status, tm.categories[catIdx], recur, recurCount);
+            tm.addTask(t);
+            if (recur && recurCount > 0) {
+                tm.addRecurringTasks(t, recurCount, 7); //repeat every 7 days
+            }
+            else if (choice == 2) {
+                tm.listTasks();
+                int idx = Utility.getInt(sc, "Index to remove: ");
+                tm.removeTask(idx);
+            }
+            else if (choice == 3) {
+                tm.listTasks();
+                int idx = Utility.getInt(sc, "Index to edit: ");
+                sx.nextLine();
+                System.out.print("New Title: ");
+                String nt = sx.nextLine();
+                System.out.print("New due dat (YYYY-MM-DD): ");
+                LocalDate nd = Utility.parseDate(sc.nextLine());
+                tm.editTask(idx, nt, nd);
+            }
+            else if (choice == 4) {
+                tm.listTasks();
+            }
+            else if (choice == 5) {
+                tm.listOverdue();
+            }
+            else if (choice == 6) {
+                tm.sortTasks();
+                System.out.println("Tasks sorted.");
+            }
+            else if (choice == 7) {
+                
+            }
            }
         }
     }
